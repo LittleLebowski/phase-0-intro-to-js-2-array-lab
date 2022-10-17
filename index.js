d63788c6fb09d58eacb81624a74a9d7e45026474
LittleLebowski <o.f.atas@gmail.com>
2022-10-17 14:52:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const cats = ["Milo", "Otis", "Garfield"];
 const destructivelyAppendCat = (name) => cats.push(name);
 const destructivelyPrependCat = (name) => cats.unshift(name);
 const destructivelyRemoveLastCat = () => cats.pop();
-const destructivelyRemoveFirstCat = () => cats.unshift();
-const appendCat = (name, arr) => {
-  const newArr = [...arr];
+const destructivelyRemoveFirstCat = () => cats.shift();
+const appendCat = (name) => {
+  const newArr = [...cats];
   newArr.push(name);
-  console.log(newArr);
   return newArr;
 };
-const prependCat = (name) => {};
-const removeLastCat = () => {};
-const removeFirstCat = () => {};
+const prependCat = (name) => {
+  const newArr = [...cats];
+  newArr.unshift(name);
+  return newArr;
+};
+const removeLastCat = () => {
+  const newArr = [...cats];
+  newArr.pop();
+  return newArr;
+};
+const removeFirstCat = () => {
+  const newArr = [...cats];
+  newArr.shift();
+  return newArr;
+};
 
 const newCat = appendCat("hop", cats);
 
