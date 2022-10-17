14e5b477e9aee508d41deecec9cc13ae8d2b32f4
LittleLebowski <o.f.atas@gmail.com>
2022-10-17 14:36:14 +0300

    bla

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+//const newCats = [];
+const destructivelyAppendCat = (name) => cats.push(name);
+const destructivelyPrependCat = (name) => cats.unshift(name);
+const destructivelyRemoveLastCat = () => cats.pop();
+const destructivelyRemoveFirstCat = () => cats.unshift();
+const appendCat = (name, arr) => {
+  const newArr = [...arr];
+  newArr.push(name);
+  console.log(newArr);
+  return newArr;
+};
+const prependCat = (name) => {};
+const removeLastCat = () => {};
+const removeFirstCat = () => {};
+
+const newCat = appendCat("hop", cats);
+
+console.log(cats);
+console.log(newCat);
